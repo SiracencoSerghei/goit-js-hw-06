ecd3d16e06e89a24060f65d7f82515f5b425b1fe
Siracenco Serghei <siracencoserghei@gmail.com>
2023-02-18 12:35:47 +0100

    add eighth resolved task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Please fill all fields");
+  }
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    console.log(`${name}: ${value};`);
+  });
+
+  event.currentTarget.reset();
+
+
+// console.dir(HTMLFormElement);
+//   console.log(formData);
+//   console.dir(event.currentTarget.elements);
+}
