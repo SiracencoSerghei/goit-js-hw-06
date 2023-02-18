223e7b947375427f6343c478662e546f78d4a6aa
Siracenco Serghei <siracencoserghei@gmail.com>
2023-02-18 13:02:02 +0100

    add ninth resolved task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', onClickChangeColorButton);
+
+function onClickChangeColorButton () {
+  colorValue.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue.textContent;
+}
