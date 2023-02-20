f0f297048a0d6e43abde1e83f620744e188ecbbc
Siracenco Serghei <siracencoserghei@gmail.com>
2023-02-20 17:36:40 +0100

    add some exemples for function resolves...

--- js/task-08.js
+++ js/task-08.js
@@ -31,3 +31,74 @@ return;
   console.log(formValues);
   event.currentTarget.reset();
 }
+
+//  ----------  variant 2  --------------------------
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const email = event.currentTarget.querySelector('input[name="email"]');
+//   const password = event.currentTarget.querySelector('input[name="password"]');
+
+//   if (email.value === "" || password.value === "") {
+//     alert("Please fill all fields");
+//     return;
+//   }
+
+//   const formData = new FormData(event.currentTarget);
+
+  // const formValues = {};
+  // formData.forEach((value, key) => formValues[key] = value);
+
+//   console.log(formValues);
+//   event.currentTarget.reset();
+// }
+
+// ---------------  variant 3   --------------
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const email = event.currentTarget.querySelector('input[name="email"]');
+//   const password = event.currentTarget.querySelector('input[name="password"]');
+
+//   if (email.value === "" || password.value === "") {
+//     alert("Please fill all fields");
+//     return;
+//   }
+
+//   const formData = new FormData(event.currentTarget);
+
+//   const formValues = {};
+//   for (let [key, value] of formData.entries()) {
+//     formValues[event.currentTarget.elements[key].getAttribute("name")] = value;
+//   }
+
+//   console.log(formValues);
+//   event.currentTarget.reset();
+// }
+
+// ---------------  variant 4   --------------
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//     const {
+//       elements: { email, password },
+//     } = event.currentTarget;
+
+//     if (email.value === "" || password.value === "") {
+//       alert("Please fill all fields");
+//   return;
+//     }
+
+//   const formData = new FormData(event.currentTarget);
+
+//   const formValues = {};
+//   for (let [key, value] of formData.entries()) {
+//     formValues[event.currentTarget.elements[key].getAttribute("name")] = value;
+//   }
+
+//   console.log(formValues);
+//   event.currentTarget.reset();
+// }
